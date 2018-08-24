838f638b5ac543f10dfbbe12c29644984af6bd6e
GonzaCampos <gonzacampos@github.com>
2018-08-24 13:34:49 -0300

    Agregado la vcp y las subnets privada y publica

--- main.tf
+++ main.tf
@@ -12,3 +12,29 @@ provider "aws" {
   shared_credentials_file = "/Users/tf_user/.aws/creds"
   region     = "us-west-2"
 }
+
+
+resource "aws_vcp" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags {
+    Name = "dinopractica"
+  }
+}
+
+resource "aws_subnet" "subnet_public" {
+  vpc_id = "${aws_vcp.main.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags {
+    Name = "subnet_public"
+  }
+}
+
+resource "aws_subnet" "subnet_private" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "subnet_private"
+  }
+}
